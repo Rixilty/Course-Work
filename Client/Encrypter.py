24d5c6fe72dc8af436f637d55a39e6d3be9246aa
Rixilty <vzb8280@gmail.com>
2025-03-03 13:41:12 +0000

    New Generate_Prime_Numbers function
    This function is used to generate 2 different random prime numbers. p and q are then set to on of these 2 prime numbers. n and phi are then calculated from the given p and q.

--- Client/Encrypter.py
+++ Client/Encrypter.py
@@ -16,7 +16,18 @@ Lock = 0
 Key = 0
 
 def Generate_Prime_Numbers():
-    pass
+    global p, q
+    for i in range(2):
+        num = random.randint(1000, 9999)
+        while is_prime(num) == False:
+            num = random.randint(1000, 9999)
+        if p == 0:
+            p = num
+        else:
+            q = num
+    n = p * q
+    phi = (p - 1) * (q - 1)
+
 
 def is_prime(num):
     pass
