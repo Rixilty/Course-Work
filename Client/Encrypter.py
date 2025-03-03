baab41624ff8a1e6b6a0d589b227a9403707bb13
Rixilty <vzb8280@gmail.com>
2025-03-03 13:39:24 +0000

    Initial commit

---
+++ Client/Encrypter.py
@@ -0,0 +1,22 @@
+import random
+
+p = 0
+q = 0
+n = 0
+phi = 0
+e = []
+d = 0
+
+Prime_Numbers = []
+
+n_Factors = []
+phi_Factors = []
+
+Lock = 0
+Key = 0
+
+def Generate_Prime_Numbers():
+    pass
+
+def is_prime(num):
+    pass
