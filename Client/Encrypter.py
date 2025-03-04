f37b023d7088a891df1659135a385e609f24e994
Rixilty <vzb8280@gmail.com>
2025-03-04 13:59:01 +0000

    New ready function
    This function is executed once the program is started. This is a more organised way that controls the flow of the program.

--- Client/Encrypter.py
+++ Client/Encrypter.py
@@ -13,6 +13,13 @@ phi_Factors = []
 Lock = 0
 Key = 0
 
+def ready():
+    Generate_Prime_Numbers()
+    Get_e()
+    Get_Factors_Of_n()
+    Get_Factors_Of_phi()
+    Get_Non_Coprime_Numbers()
+
 def Generate_Prime_Numbers():
     global p, q
     for i in range(2):
@@ -26,34 +33,28 @@ def Generate_Prime_Numbers():
     n = p * q
     phi = (p - 1) * (q - 1)
     print("p:",p, "q:",q, "n:",n, "phi:",phi)
-    Get_e()
-
 
 def is_prime(num):
     if num % 2 == 0 or num <= 1:
         return False
     return True
 
-
 def Get_e():
     for i in range(2, phi+1):
         e.append(i)
     print("e:",e)
-    Get_Factors_Of_n()
 
 def Get_Factors_Of_n():
     for i in range(1, n+1):
         if n % i == 0 and i != 1:
             n_Factors.append(i)
     print("n_Factors:",n_Factors)
-    Get_Factors_Of_phi()
 
 def Get_Factors_Of_phi():
     for i in range(1, phi+1):
         if phi%i == 0 and i != 1:
             phi_Factors.append(i)
     print("phi_Factors:",phi_Factors)
-    Get_Non_Coprime_Numbers()
 
 def Get_Non_Coprime_Numbers():
     for i in range(len(e)):
@@ -64,3 +65,5 @@ def Get_Non_Coprime_Numbers():
            if e[i] % phi_Factors[j] == 0:
                e.remove(e[i])
     print("e:",e)
+
+ready()
