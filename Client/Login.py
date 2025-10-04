1cb55cb521ebb70bd8ea4807925b950c596ce8d2
Rixilty <117123299+Rixilty@users.noreply.github.com>
2025-10-04 03:17:23 +0100

    New Validate_password function
    This function further checks and validates the password making sure it contains at least:
    
    - 1 Uppercase letter
    - 1 Lowercase letter
    - 1 Number
    - 1 Symbol
    
    This avoids the user from entered a weak password that could easily guessed.

--- Client/Login.py
+++ Client/Login.py
@@ -31,6 +31,32 @@ class Login_GUI:
         elif (len(Password) < 8) or (len(Password) > 16):
             messagebox.showerror("Error", "Password length invalid! (8-16)")
         else:
-            messagebox.showinfo("Success", "Login Successful")
+            self.Validate_password(Password)
+            if self.Validate_password(Password):
+                messagebox.showinfo("Success", "Login Successful")
+
+    def Validate_password(self, password):
+        Validity = [False, False, False, False]
+        for char in password:
+            if char == char.upper(): # Checks for uppercase letters
+                Validity[0] = True
+            if char == char.lower(): # Checks for lowercase letters
+                Validity[1] = True
+            if char.isdigit(): # Checks for numbers
+                Validity[2] = True
+            if (char != char.ischar()) and (char != char.isdigit()): # Checks for symbols
+                Validity[3] = True
+
+        if not Validity[0]:
+            return messagebox.showerror("Error", "Password must contain at least 1 uppercase letter!")
+        elif not Validity[1]:
+            return messagebox.showerror("Error", "Password must contain at lest 1 lowercase letter!")
+        elif not Validity[2]:
+            return messagebox.showerror("Error", "Password must contain at least 1 number!")
+        elif not Validity[3]:
+            return messagebox.showerror("Error", "Password must contain at least 1 symbol!")
+        else:
+            return True
+
 
 Login_GUI()
