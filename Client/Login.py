c423a6f71ba69d167a616c3955ca2b79a014544a
Rixilty <117123299+Rixilty@users.noreply.github.com>
2025-11-14 19:05:02 +0000

    Updated Login GUI
    I changed the size and font of the "Login" text for the title, "Username" text for the username label, "Password" for the password label, I also made them bold so they look better. I removed the padding between the labels and the entries, as it looks better. I also changed the size and font for the "Don't already have an account" text and the "Sign up." text and made them bold. I also underlined the "Sign up" text, so it looks more like a hyperlink and closer to my mock-up. I also fixed a typo "Dont't" to "Don't".

--- Client/Login.py
+++ Client/Login.py
@@ -26,21 +26,21 @@ class Login_GUI:
         self.main_frame.grid_columnconfigure(0, weight=1)
         self.main_frame.grid_columnconfigure(1, weight=1)
 
-        self.Title = tk.Label(self.main_frame, text="Login", font=("Arial", 16), fg="white", bg="#1c1c1c")
+        self.Title = tk.Label(self.main_frame, text="Login", font=("Arial", 24, "bold"), fg="white", bg="#1c1c1c")
         self.Title.grid(row=0, column=0, columnspan=2, pady=20)
 
-        self.Username_label = tk.Label(self.main_frame, text="Username:", fg="white", bg="#1c1c1c")
-        self.Username_label.grid(row=1, column=0, sticky="e", padx=10)
+        self.Username_label = tk.Label(self.main_frame, text="Username:", font=("Arial",10, "bold"), fg="white", bg="#1c1c1c")
+        self.Username_label.grid(row=1, column=0, sticky="e")
 
         self.Username_entry = tk.Entry(self.main_frame, width=25)
-        self.Username_entry.grid(row=1, column=1, padx=10)
+        self.Username_entry.grid(row=1, column=1)
         self.add_placeholder(self.Username_entry, "Username")
 
-        self.Password_label = tk.Label(self.main_frame, text="Password:", fg="white", bg="#1c1c1c")
-        self.Password_label.grid(row=2, column=0, sticky="e", padx=10)
+        self.Password_label = tk.Label(self.main_frame, text="Password:", font=("Arial",10, "bold"), fg="white", bg="#1c1c1c")
+        self.Password_label.grid(row=2, column=0, sticky="e")
 
         self.Password_entry = tk.Entry(self.main_frame, width=25, show="*")
-        self.Password_entry.grid(row=2, column=1, padx=10)
+        self.Password_entry.grid(row=2, column=1)
         self.add_placeholder(self.Password_entry, "Password", is_password=True)
 
         self.Login_button = tk.Button(self.main_frame, text="Login", command=self.Login)
@@ -49,10 +49,10 @@ class Login_GUI:
         bottom_frame = tk.Frame(self.main_frame, bg=self.main_frame["bg"])
         bottom_frame.grid(row=4, column=0, columnspan=2, pady=20)
 
-        self.not_signed_label = tk.Label(bottom_frame, text="Dont't have an account?", fg="white", bg="#1c1c1c")
+        self.not_signed_label = tk.Label(bottom_frame, text="Don't have an account?", font=("Arial",10, "bold"), fg="white", bg="#1c1c1c")
         self.not_signed_label.grid(row=0, column=0)
 
-        self.signup_link = tk.Label(bottom_frame, text="Sign up.", fg="#001eff", bg="#1c1c1c", cursor="hand2")
+        self.signup_link = tk.Label(bottom_frame, text="Sign up.", font=("Arial",10, "bold underline"), fg="#001eff", bg="#1c1c1c", cursor="hand2")
         self.signup_link.grid(row=0, column=1, padx=0)
 
         self.signup_link.bind("<Button-1>", self.on_signup_click)
