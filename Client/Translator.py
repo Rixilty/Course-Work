2add2ff31ac693af1a10181c15ddc216679e7c2c
Rixilty <117123299+Rixilty@users.noreply.github.com>
2026-02-03 23:16:14 +0000

    Translator Fixed
    I used await because network requests to google Translate take time. Without async and running the next function without having the result from the previous function causes an error. This is also useful as the login window would freeze and stop responding to clicks until the translation is complete.
    
    Async defines the function as a coroutine. It doesn't run immediately when it is called.
    
    Await tells the program to wait for the result of the coroutine before continuing, while it waits the rest of the application can stay responsive.
    
    This is all managed by an Asynchronous function "asyncio.run()", which creates an Event Loop that schedules and runs these paused tasks. It constantly checks if a network request is finished and continues the paused function once data arrives.

--- Client/Translator.py
+++ Client/Translator.py
@@ -1,18 +1,31 @@
+import asyncio
 from googletrans import Translator
 
 translator = Translator()
 
-message = "Hello"
+message = "Hallo Welt!"
 destination = "en"
 
-def detect_language(text):
-    lang_detect = translator.detect(text)
-    return lang_detect.lang
+# Define as an async function
+async def detect_language(text):
+    # await the coroutine to get the actual result
+    lang_detect = await translator.detect(text) # detect the language of the text
+    return lang_detect.lang # return the detected language
 
-def translate(text):
-    source = detect_language(text)
+async def translate(text):
+    source = await detect_language(text)
     if source != destination:
-        translated_message = translator.translate(text, dest='en', src=source)
-        return translated_message.text
+        # await translation result
+        translated_message = await translator.translate(text, dest='en', src=source)
+        return translated_message.text # return translated messsage
+    return text #  return the original text if it's already in the destination language
 
-translate(message)
+# A main function to run the async code
+async def main():
+    language_detected = await detect_language(message)
+    result = await translate(message)
+    print("Language detected:", language_detected)
+    print("Translated text:", result)
+
+if __name__ == "__main__":
+    asyncio.run(main())
